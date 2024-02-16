8d7d52bb143c38f7c72d0438718a2a5ad058aae8
Chris-xun <yx20021225@163.com>
2024-02-16 16:12:52 +0000

    data

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import functions as f
 import matplotlib.pyplot as plt
 
 
-date, time, intensity, temp = f.import_data(r'data\LOG240215-0945.csv')
+date, time, intensity, temp = f.import_data(r'data\LOG240216-0934.csv')
 
 f.plot_data(time, intensity, temp)
 
